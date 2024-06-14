1f40aa8dbc94881ade990d950f939c37d425c517
barnabas <barnabas.mwendwa@centrino.co.ke>
2024-06-14 12:28:21 +0300

    Add auth page styling

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,23 +1,26 @@
-"use-client";
+import "../style.css";
+("use-client");
 
 const SignIn = () => {
   return (
-    <main className="wrapper-padding content-wrapper">
-      <div className="forms">
-        <h1>Sign in</h1>
-        <form>
-          <div className="form-group mt-3">
-            <label htmlFor="name">Email</label>
-            <input type="email" className="mt-2" name="name" />
-          </div>
-          <div className="form-group mt-3">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="mt-2" name="password" />
-          </div>
-          <button type="submit" className="primary-cta mt-3 w-100">
-            Sign in
-          </button>
-        </form>
+    <main className="wrapper-padding">
+      <div className="d-flex">
+        <div className="forms">
+          <h1>Sign in</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Email</label>
+              <input type="email" name="name" />
+            </div>
+            <div className="form-group mt-3">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+            </div>
+            <button type="submit" className="primary-cta mt-3 w-100">
+              Sign in
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
